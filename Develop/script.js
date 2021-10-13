3e440abba41ea7b7b32d0c51b6bc700be575d68d
tiana <tianasjohnson98@gmail.com>
2021-10-13 14:22:01 -0400

    adding comments to the assigned variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,26 @@
 // Assignment code here
-var char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+// alphabet range for password
+var alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//numeric values for password
 var num = "0123456789";
+
+//symbols for password
 var sym = "!@#$%^&*+=-_";
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+ 
+ passLength = window.prompt("write a number between 8 and 132  for your password");
+  
+ 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
 }
@@ -22,4 +32,9 @@ function generatePassword () {
 }
 
 // Add event listener to generate button
+
+
+
+
+
 generateBtn.addEventListener("click", writePassword);
