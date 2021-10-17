9decad72a7a2c4ed6813e00493ed31653e15d916
tiana <tianasjohnson98@gmail.com>
2021-10-17 16:25:16 -0400

    generate passwprd function

--- Develop/script.js
+++ Develop/script.js
@@ -1,40 +1,58 @@
 // Assignment code here
-
-// alphabet range for password
-var alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//numeric values for password
-var num = "0123456789";
-
-//symbols for password
-var sym = "!@#$%^&*+=-_";
+var generateBtn = document.querySelector("#generate");
+  
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+
+
+
+function generatePassword(){
+
+   window.confirm("Would you like upper case letters?");
+   window.confirm("Would you like numbers?");
+   window.confirm("Would you like lower case letters?");
+   window.confirm("Would you like symbols?");
 
 // Write password to the #password input
+var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTRUVWXYZ";
+var passwordLength = 20;
+var pass = "";
+for (var i = 0; i <= passwordLength; i++){
+ var numberRandom = Math.floor(Math.random() * chars.length);
+ pass += chars.substring(numberRandom , numberRandom +1);
+}
+return pass;
+}
+
+
+
+
+
 function writePassword() {
- 
- passLength = window.prompt("write a number between 8 and 132  for your password");
-  
+
  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   
   passwordText.value = password;
-
 }
 
-function generatePassword () {
+//reveal the password onto the input
 
-  
 
-}
 
 // Add event listener to generate button
 
 
-
-
-
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+
